3d2fd8e70a0ce1a48554b0e3ea428bcd421ea902
Sudeep U <sudeepu@Sudeeps-MacBook-Air.local>
2025-08-18 23:50:13 +0530

    zipkin_micrometer

--- api-gateway/src/main/java/config/AppConfig.java
+++ api-gateway/src/main/java/config/AppConfig.java
@@ -1,19 +1,13 @@
-package com.microservices.inventory.config;
+package config;
 
 import feign.Capability;
 import feign.micrometer.MicrometerCapability;
 import io.micrometer.core.instrument.MeterRegistry;
-import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
-    @Bean
-    public ModelMapper modelMapper(){
-        return new ModelMapper();
-    }
-
     @Bean
     public Capability capability(final MeterRegistry registry) {
         return new MicrometerCapability(registry);
--- product/src/main/java/com/microservices/product/config/AppConfig.java
+++ product/src/main/java/com/microservices/product/config/AppConfig.java
@@ -1,5 +1,8 @@
 package com.microservices.product.config;
 
+import feign.Capability;
+import feign.micrometer.MicrometerCapability;
+import io.micrometer.core.instrument.MeterRegistry;
 import lombok.Builder;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
@@ -11,5 +14,10 @@ public class AppConfig {
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+    @Bean
+    public Capability capability(final MeterRegistry registry) {
+        return new MicrometerCapability(registry);
     }
 }
