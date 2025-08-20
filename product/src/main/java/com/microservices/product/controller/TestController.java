b99e12a1fd5ebf27cfad7f76af3142514bd6226b
Sudeep U <sudeepu@Sudeeps-MacBook-Air.local>
2025-08-21 02:16:30 +0530

    configserver

--- product/src/main/java/com/microservices/product/controller/TestController.java
+++ product/src/main/java/com/microservices/product/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.microservices.product.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,5 +10,13 @@ public class TestController {
     @GetMapping("/test")
     public String test()  {
         return "Test successful!";
+    }
+
+    @Value("${app.message}")
+    private String message;
+
+    @GetMapping("/message")
+    public String getMessage() {
+        return message;
     }
 }
